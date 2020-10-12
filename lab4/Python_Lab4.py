fccadea596ebd736e260b2d9cfacc96d49779dbf
Efimov Viacheslav <efimovvo@yandex.ru>
2020-10-12 11:43:17 +0300

    Made the score increasing and decreasing when object touch the ceiling

--- lab4/Python_Lab4.py
+++ lab4/Python_Lab4.py
@@ -1,19 +1,23 @@
 import pygame
 from pygame.draw import *
-from random import randint
+from random import randint, random
 
 def draw_the_balls(balls):
 	for ball in balls:
-		circle(screen, ball['color'], (ball['x'], ball['y']), ball['r'])
+		circle(screen, ball['color'], (int(ball['x']), int(ball['y'])), int(ball['r']))
+
+def draw_the_score(score):
+	f1 = pygame.font.Font(None, 36)
+	text_score = f1.render('Score: ' + str(int(score)), 1, DARK_RED)
+	screen.blit(text_score, (10, 40))
 
 def create_ball():
 	global x, y, v_x, v_y, r, color
 	x = randint(100, screen_size[0] - 100)
 	y = randint(100, screen_size[1] - 100)
-	v_x = randint(-10, 10)
-	v_y = randint(-10, 10)
-	print(v_y)
-	r = randint(30, 50)
+	v_x = 2*max_ball_speed*(random() - 0.5)
+	v_y = 2*max_ball_speed*(random() - 0.5)
+	r = randint(max_ball_radius // 2, max_ball_radius)
 	color = COLORS[randint(0, 5)]
 	return {'x': x, 'y': y, 'v_x': v_x, 'v_y': v_y, 'r': r, 'color': color}
 
@@ -27,14 +31,21 @@ def change_background(color):
 	return color
 
 def click(event):
-	global background_color
+	global background_color, score
+
 	for ball in balls:
 		distance = ((event.pos[0] - ball['x'])**2 + (event.pos[1] - ball['y'])**2)**0.5
 		if (distance <= ball['r']):
+			score += 100 // ball['r']
 			background_color = change_background(background_color)
+			ball['r'] /= 1.5
+			ball['v_x'] /= 2
+			ball['v_y'] /= 2
+			break
 	pygame.display.update()
 
 def move_the_ball(ball):
+	global score
 	ball['x'] += ball['v_x'] * time_step
 	max_x = screen_size[0] - ball['r']
 	min_x = ball['r']
@@ -45,6 +56,7 @@ def move_the_ball(ball):
 		ball['x'] = 2*min_x - ball['x']
 		ball['v_x'] *= -1
 
+	ball['y'] += ball['v_y'] * time_step
 	max_y = screen_size[1] - ball['r']
 	min_y = ball['r']
 	if ball['y'] > max_y:
@@ -53,13 +65,14 @@ def move_the_ball(ball):
 	elif ball['y'] < min_y:
 		ball['y'] = 2*min_x - ball['y']
 		ball['v_y'] *= -1
+		score -= ball['r'] / 4
 	return ball
 
 
 pygame.init()
 
-FPS = 1
-screen_size = (1000, 800)
+FPS = 10
+screen_size = (1000, 600)
 screen = pygame.display.set_mode(screen_size)
 
 RED = (255, 0, 0)
@@ -70,6 +83,9 @@ MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+
+DARK_RED = (100, 0, 0)
+
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 pygame.display.update()
@@ -78,9 +94,15 @@ finished = False
 
 number_of_balls = 5
 balls = []
+max_ball_radius = 50
+max_ball_speed = 3
+score = 0
+
+draw_the_score(0)
+
 for i in range(number_of_balls):
 	balls += [create_ball()]
-	draw_the_balls(balls[i])
+	draw_the_balls([balls[i]])
 background_color = BLACK
 time_step = FPS
 
@@ -89,12 +111,17 @@ while not finished:
 	screen.fill(background_color)
 	for ball in balls:
 		move_the_ball(ball)
-		draw_the_ball(ball)
+	draw_the_balls(balls)
+	draw_the_score(score)
 	pygame.display.update()
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			finished = True
 		elif event.type == pygame.MOUSEBUTTONDOWN:
 			click(event)
+
+	for i in range(len(balls)):
+		if balls[i]['r'] < max_ball_radius / 5:
+			balls[i] = create_ball()
 
 pygame.quit()
